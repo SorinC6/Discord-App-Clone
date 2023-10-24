e863ec71deb16b7ac925775db8019b1295df5d38
SorinC6 <kish.sorin@yahoo.com>
2023-10-24 17:59:44 +0300

    add authentification

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,17 +1,11 @@
+import { UserButton } from "@clerk/nextjs";
 import { Button } from "../../../components/ui/button";
 import { cn } from "../../../lib/utils";
 
 export default function Home() {
   return (
     <div>
-      <p className="text-3xl font-bold text-indigo-500">Hello</p>
-
-      <Button
-        variant="ghost"
-        className={cn("bg-red-400", false && "bg-slate-600")}
-      >
-        Press me
-      </Button>
+      <UserButton afterSignOutUrl="/" />
     </div>
   );
 }
