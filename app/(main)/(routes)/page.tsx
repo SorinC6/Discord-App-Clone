70f9fb8e9a22b029a8c8fad4ea7941afec834ef1
SorinC6 <kish.sorin@yahoo.com>
2023-10-24 18:22:11 +0300

    add light/dark theme provider

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,11 +1,13 @@
 import { UserButton } from "@clerk/nextjs";
 import { Button } from "../../../components/ui/button";
 import { cn } from "../../../lib/utils";
+import { ModeToggle } from "../../../components/mode-toggle";
 
 export default function Home() {
   return (
     <div>
       <UserButton afterSignOutUrl="/" />
+      <ModeToggle />
     </div>
   );
 }
