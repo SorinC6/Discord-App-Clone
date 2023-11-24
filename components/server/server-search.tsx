df3b4926b9cd0cef271309faec6f4a2867e72488
SorinC6 <kish.sorin@yahoo.com>
2023-11-24 15:37:55 +0200

    added text channel search feature

---
+++ components/server/server-search.tsx
@@ -0,0 +1,88 @@
+'use client';
+import React from 'react';
+import { Search } from 'lucide-react';
+import { CommandDialog, CommandEmpty, CommandInput, CommandList, CommandGroup } from '../ui/command';
+import { CommandItem } from 'cmdk';
+import { useParams, useRouter } from 'next/navigation';
+
+interface ServerSearchProps {
+  data:
+    | {
+        label: string;
+        type: 'channel' | 'member';
+        data:
+          | {
+              icon: React.ReactNode;
+              name: string;
+              id: string;
+            }[]
+          | undefined;
+      }[]
+    | undefined;
+}
+
+export const ServerSearch = ({ data }: ServerSearchProps) => {
+  const [open, setOpen] = React.useState(false);
+  const router = useRouter();
+  const params = useParams();
+
+  React.useEffect(() => {
+    const down = (e: KeyboardEvent) => {
+      if (e.key === 'k' && (e.metaKey || e.ctrlKey)) {
+        e.preventDefault();
+        setOpen(true);
+      }
+    };
+    document.addEventListener('keydown', down);
+
+    return () => document.removeEventListener('keydown', down);
+  }, []);
+
+  const onClick = ({ id, type }: { id: string; type: 'channel' | 'member' }) => {
+    setOpen(false);
+    if (type === 'member') {
+      router.push(`/servers/${params.serverId}/conversations/${id}`);
+    }
+    if (type === 'channel') {
+      router.push(`/servers/${params.serverId}/channels/${id}`);
+    }
+  };
+
+  return (
+    <>
+      <button
+        onClick={() => setOpen(true)}
+        className="flex items-center w-full px-2 py-2 transition rounded-md group gap-x-2 hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50"
+      >
+        <Search className="h-4 2-4 text-zinc-500 dark:text-zinc-400" />
+        <p className="text-sm font-semibold transition text-zinc-500 dark:text-zinc-400 group-hover:text-zonc-600 dark:hover:text-zinc-600 dark:group-hover-text-zinc-300">
+          Search
+        </p>
+        <kbd className="inline-flex h-5 pointer-events-none select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground ml-auto">
+          <span className="text-xs">⌘</span>K
+        </kbd>
+      </button>
+      <CommandDialog open={open} onOpenChange={setOpen}>
+        <CommandInput placeholder="Search all channels and members" />
+        <CommandList>
+          <CommandEmpty>No results found</CommandEmpty>
+          {data?.map(({ label, type, data }) => {
+            if (!data?.length) return null;
+            return (
+              <CommandGroup key={label} heading={label}>
+                {data?.map(({ id, icon, name }) => {
+                  return (
+                    <CommandItem key={id} className="flex items-center" onSelect={() => onClick({ id, type })}>
+                      {icon}
+                      <span>{name}</span>
+                    </CommandItem>
+                  );
+                })}
+              </CommandGroup>
+            );
+          })}
+        </CommandList>
+      </CommandDialog>
+    </>
+  );
+};
--- components/server/server-sidebar.tsx
+++ components/server/server-sidebar.tsx
@@ -1,9 +1,27 @@
-import React from "react";
-import { currentProfile } from "../../lib/current-profile";
-import { db } from "../../lib/db";
-import { ChannelType } from "@prisma/client";
-import { redirect } from "next/navigation";
-import { ServerHeader } from "./server-header";
+import React from 'react';
+import { currentProfile } from '../../lib/current-profile';
+import { db } from '../../lib/db';
+import { ChannelType, MemberRole } from '@prisma/client';
+import { redirect } from 'next/navigation';
+import { ServerHeader } from './server-header';
+import { ScrollArea } from '../ui/scroll-area';
+import { ServerSearch } from './server-search';
+import { Hash, Mic, ShieldAlert, ShieldCheck, Video } from 'lucide-react';
+import { Separator } from '../ui/separator';
+import { ServerSection } from './server-section';
+import { ServerChannel } from './server-channel';
+
+const iconMap = {
+  [ChannelType.TEXT]: <Hash className="h-4 mr-2 2-4" />,
+  [ChannelType.AUDIO]: <Mic className="h-4 mr-2 2-4" />,
+  [ChannelType.VIDEO]: <Video className="h-4 mr-2 2-4" />,
+};
+
+const roleIconMap = {
+  [MemberRole.GUEST]: null,
+  [MemberRole.MODERATOR]: <ShieldCheck className="w-4 h-4 ml-2 text-indigo-500" />,
+  [MemberRole.ADMIN]: <ShieldAlert className="w-4 h-4 mr-2 text-rose-500" />,
+};
 
 export const ServerSidebar = async ({ serverId }: { serverId: string }) => {
   const profile = await currentProfile();
@@ -15,7 +33,7 @@ export const ServerSidebar = async ({ serverId }: { serverId: string }) => {
     include: {
       channels: {
         orderBy: {
-          createdAt: "asc",
+          createdAt: 'asc',
         },
       },
       members: {
@@ -23,38 +41,81 @@ export const ServerSidebar = async ({ serverId }: { serverId: string }) => {
           profile: true,
         },
         orderBy: {
-          role: "asc",
+          role: 'asc',
         },
       },
     },
   });
 
-  const textChannels = server?.channels.filter(
-    (ch) => ch.type === ChannelType.TEXT
-  );
+  const textChannels = server?.channels.filter((ch) => ch.type === ChannelType.TEXT);
 
-  const audioChannels = server?.channels.filter(
-    (ch) => ch.type === ChannelType.AUDIO
-  );
+  const audioChannels = server?.channels.filter((ch) => ch.type === ChannelType.AUDIO);
 
-  const videoChannels = server?.channels.filter(
-    (ch) => ch.type === ChannelType.VIDEO
-  );
-  const members = server?.members.filter(
-    (member) => member.profileId !== profile.id
-  );
+  const videoChannels = server?.channels.filter((ch) => ch.type === ChannelType.VIDEO);
+  const members = server?.members.filter((member) => member.profileId !== profile.id);
 
   if (!server) {
-    return redirect("/");
+    return redirect('/');
   }
 
-  const role = server.members.find(
-    (member) => member.profileId === profile.id
-  )?.role;
+  const role = server.members.find((member) => member.profileId === profile.id)?.role;
 
   return (
     <div className="flex flex-col h-full text-primary w-full dark:bg-[#2b2d31] bg-[#f2f3f5]">
       <ServerHeader server={server} role={role} />
+      <ScrollArea className="flex-1 px-3">
+        <div className="mt-2">
+          <ServerSearch
+            data={[
+              {
+                label: 'Text Channels',
+                type: 'channel',
+                data: textChannels?.map((channel) => ({
+                  id: channel.id,
+                  name: channel.name,
+                  icon: iconMap[channel.type] || null,
+                })),
+              },
+              {
+                label: 'Voice Channels',
+                type: 'channel',
+                data: audioChannels?.map((channel) => ({
+                  id: channel.id,
+                  name: channel.name,
+                  icon: iconMap[channel.type],
+                })),
+              },
+              {
+                label: 'Video Channels',
+                type: 'channel',
+                data: videoChannels?.map((channel) => ({
+                  id: channel.id,
+                  name: channel.name,
+                  icon: iconMap[channel.type],
+                })),
+              },
+              {
+                label: 'Members Channels',
+                type: 'member',
+                data: members?.map((member) => ({
+                  id: member.id,
+                  name: member.profile.name,
+                  icon: roleIconMap[member.role],
+                })),
+              },
+            ]}
+          />
+        </div>
+        <Separator className="my-2 rounded-md bg-zinc-200 dark:bg-zinc-700" />
+        {!!textChannels?.length && (
+          <div className="mb-2">
+            <ServerSection sectionType="channels" channelType={ChannelType.TEXT} role={role} label="Text Channels" />
+            {textChannels.map((channel) => (
+              <ServerChannel key={channel.id} channel={channel} role={role} server={server} />
+            ))}
+          </div>
+        )}
+      </ScrollArea>
     </div>
   );
 };
