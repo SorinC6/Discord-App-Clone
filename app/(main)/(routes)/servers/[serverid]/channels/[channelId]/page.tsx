a23338740e325abd0e7ab8c9ac5bf3c678c9f28d
SorinC6 <kish.sorin@yahoo.com>
2023-11-29 15:32:39 +0200

    added chat header and mobile side bar

--- app/(main)/(routes)/servers/[serverid]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverid]/channels/[channelId]/page.tsx
@@ -1,7 +1,44 @@
 import React from 'react';
+import { currentProfile } from '@/lib/current-profile';
+import { redirectToSignIn } from '@clerk/nextjs';
+import { db } from '@/lib/db';
+import { redirect } from 'next/navigation';
+import { ChatHeader } from '@/components/chat/chat-header';
 
-const ChannelIdPage = () => {
-  return <div>CahnnelId</div>;
+interface ChanneIdPageProps {
+  params: {
+    serverId: string;
+    channelId: string;
+  };
+}
+const ChannelIdPage = async ({ params }: ChanneIdPageProps) => {
+  const profile = await currentProfile();
+  if (!profile) {
+    return redirectToSignIn();
+  }
+
+  const channel = await db.channel.findUnique({
+    where: {
+      id: params.channelId,
+    },
+  });
+
+  const member = await db.member.findFirst({
+    where: {
+      serverId: params.serverId,
+      profileId: profile.id,
+    },
+  });
+
+  if (!channel || !member) {
+    redirect('/');
+  }
+
+  return (
+    <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
+      <ChatHeader name={channel.name} serverId={channel.serverId} type="channel" />
+    </div>
+  );
 };
 
 export default ChannelIdPage;
