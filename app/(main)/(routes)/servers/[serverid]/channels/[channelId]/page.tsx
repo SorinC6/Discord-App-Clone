d928df2c1495a10810c4af7b3289a8a765cd50ae
SorinC6 <kish.sorin@yahoo.com>
2023-12-05 15:51:40 +0200

    add chat input

--- app/(main)/(routes)/servers/[serverid]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverid]/channels/[channelId]/page.tsx
@@ -4,6 +4,7 @@ import { redirectToSignIn } from '@clerk/nextjs';
 import { db } from '@/lib/db';
 import { redirect } from 'next/navigation';
 import { ChatHeader } from '@/components/chat/chat-header';
+import { ChatInput } from '../../../../../../../components/chat/chat-input';
 
 interface ChanneIdPageProps {
   params: {
@@ -37,6 +38,17 @@ const ChannelIdPage = async ({ params }: ChanneIdPageProps) => {
   return (
     <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
       <ChatHeader name={channel.name} serverId={channel.serverId} type="channel" />
+
+      <div className="flex-1">Future Messages</div>
+      <ChatInput
+        type="channel"
+        name={channel.name}
+        apiUrl="/api/socket/messages"
+        query={{
+          channelId: channel.id,
+          serverId: channel.serverId,
+        }}
+      />
     </div>
   );
 };
