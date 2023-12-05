6aa5e417676094222eac6eca53e39a41890f8c16
SorinC6 <kish.sorin@yahoo.com>
2023-12-05 15:08:02 +0200

    add conversation setup

--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -1,6 +1,7 @@
 import { Hash } from 'lucide-react';
 import React from 'react';
 import { MobileToggle } from '../mobile-toggle';
+import { UserAvatar } from '../user-avatar';
 
 interface ChatHeaderProps {
   serverId: string;
@@ -14,6 +15,7 @@ export const ChatHeader = ({ serverId, name, type, imageUrl }: ChatHeaderProps) 
     <div className="flex items-center h-12 px-3 font-semibold border-b-2 text-md border-neutral-200 dark:border-neutral-800">
       <MobileToggle serverId={serverId} />
       {type === 'channel' && <Hash className="w-5 h-5 mr-2 text-zinc-500 dark:text-zic-400" />}
+      {type === 'conversation' && <UserAvatar src={imageUrl} className="w-8 h-8 mr-2 md:w-8 md:h-8" />}
       <p className="font-semibold text-black text-md dark:text-white">{name}</p>
     </div>
   );
