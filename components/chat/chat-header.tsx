eddd8735fe600e004ba1f89ddd814505f86514fe
SorinC6 <kish.sorin@yahoo.com>
2023-12-05 15:30:08 +0200

    add socket.io connection

--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -2,6 +2,7 @@ import { Hash } from 'lucide-react';
 import React from 'react';
 import { MobileToggle } from '../mobile-toggle';
 import { UserAvatar } from '../user-avatar';
+import { SocketIndicator } from '../socket-indicator';
 
 interface ChatHeaderProps {
   serverId: string;
@@ -17,6 +18,9 @@ export const ChatHeader = ({ serverId, name, type, imageUrl }: ChatHeaderProps) 
       {type === 'channel' && <Hash className="w-5 h-5 mr-2 text-zinc-500 dark:text-zic-400" />}
       {type === 'conversation' && <UserAvatar src={imageUrl} className="w-8 h-8 mr-2 md:w-8 md:h-8" />}
       <p className="font-semibold text-black text-md dark:text-white">{name}</p>
+      <div className="flex items-center ml-auto">
+        <SocketIndicator />
+      </div>
     </div>
   );
 };
